5157109816ccc92fb7076f38efce09dc5eee0c79
Pierre Brito <carlospierre07@gmail.com>
2026-08-21 04:44:18 -0300

    fix(prisma): explicitly inject env.DATABASE_URL into PrismaClient datasources

--- packages/api/src/core/database/prisma.ts
+++ packages/api/src/core/database/prisma.ts
@@ -6,6 +6,11 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
     log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
